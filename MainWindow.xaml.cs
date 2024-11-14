341f11c5aef02a22af462d9b1245853cbb4bda49
Nixon <blindxfish@gmail.com>
2024-11-14 07:22:47 +0100

    Initioal

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace NNote
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AlwaysOnTopButton_Checked(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = true;
+        }
+
+        private void AlwaysOnTopButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = false;
+        }
+
+        // Close button click event
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Enable dragging the window when the top area is clicked
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        // Event handler for creating a new sticky note
+        private void NewNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow newNote = new MainWindow();
+            newNote.Show();
+        }
+    }
+}
